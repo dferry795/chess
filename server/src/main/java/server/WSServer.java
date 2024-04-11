34a3d1c0da83b1b61bc328c3f4e82f12d4acd570
Darian Ferry <dferry795@gmail.com>
2024-04-11 09:42:43 -0600

    commit(phase6)

--- server/src/main/java/server/WSServer.java
+++ server/src/main/java/server/WSServer.java
@@ -2,21 +2,19 @@ package server;
 
 
 import chess.ChessGame;
+import chess.ChessMove;
 import com.google.gson.Gson;
-import dataAccess.DataAccessException;
-import dataAccess.DatabaseManager;
-import dataAccess.GameDataInterface;
-import dataAccess.SqlGameDOA;
+import dataAccess.*;
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketConnect;
 import org.eclipse.jetty.websocket.api.annotations.OnWebSocketError;
 import org.eclipse.jetty.websocket.api.annotations.OnWebSocketMessage;
 import org.eclipse.jetty.websocket.api.annotations.WebSocket;
+import service.GameService;
 import webSocketMessages.serverMessages.Error;
 import webSocketMessages.serverMessages.LoadGame;
 import webSocketMessages.serverMessages.Notification;
-import webSocketMessages.userCommands.JoinObserver;
-import webSocketMessages.userCommands.JoinPlayer;
-import webSocketMessages.userCommands.MakeMove;
-import webSocketMessages.userCommands.UserGameCommand;
+import webSocketMessages.userCommands.*;
+
 import static dataAccess.DatabaseManager.getConnection;
 
 import org.eclipse.jetty.websocket.api.Session;
@@ -25,35 +23,22 @@ import org.eclipse.jetty.websocket.api.Session;
 import java.io.IOException;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 import static java.sql.DriverManager.getConnection;
 
 @WebSocket
 public class WSServer {
-    public final ConcurrentHashMap<String, Connection> connections = new ConcurrentHashMap<>();
+    public final HashMap<String, Connection> connections = new HashMap<>();
+    public final GameService gameService = new GameService(new SqlAuthDOA(), new SqlGameDOA());
     public final GameDataInterface gameDataInterface = new SqlGameDOA();
-
 
     private void joinPlayer(JoinPlayer action, Session session) throws IOException {
         try {
-            Connection connection = new Connection(action.getUsername(), session);
-            connections.put(action.getUsername(), connection);
-            String color;
-
-            if (action.getPlayerColor() == ChessGame.TeamColor.WHITE) {
-                color = "white";
-            } else if (action.getPlayerColor() == ChessGame.TeamColor.BLACK) {
-                color = "black";
-            } else {
-                color = "observer";
-            }
-            String message = action.getAuthString() + " joined " + action.getId() + " as " + color;
-            Notification notification = new Notification(message);
-
-
-            LoadGame loadGame = new LoadGame(new ChessGame());
-            session.getRemote().sendString(new Gson().toJson(loadGame));
+            session.getRemote().sendString(new Gson().toJson(new Error("Error")));
         } catch (Throwable ex){
             session.getRemote().sendString(new Gson().toJson(new Error("Error: " + ex.getMessage())));
         }
@@ -74,10 +59,25 @@ public class WSServer {
 
     public void makeMove(MakeMove action, Session session) throws IOException {
         try {
-            Connection connection = new Connection(action.getUsername(), session);
-            connections.put(action.getUsername(), connection);
+            session.getRemote().sendString(new Gson().toJson(new Error("Error")));
         } catch (Throwable ex){
             session.getRemote().sendString(new Gson().toJson(new Error("Error: " + ex.getMessage())));
+        }
+    }
+
+    public void resign(Resign action, Session session) throws IOException {
+        try {
+            session.getRemote().sendString(new Gson().toJson(new Error("Error")));
+        } catch (Throwable ex){
+            session.getRemote().sendString(new Gson().toJson(new Error("Error")));
+        }
+    }
+
+    public void leave(Leave action, Session session) throws IOException {
+        try {
+            session.getRemote().sendString(new Gson().toJson(new Error("Error")));
+        } catch (Throwable ex){
+            session.getRemote().sendString(new Gson().toJson(new Error("Error")));
         }
     }
 
@@ -104,18 +104,22 @@ public class WSServer {
     public void onMessage(Session session, String message) throws Exception {
         UserGameCommand action = new Gson().fromJson(message, UserGameCommand.class);
 
-        var conn = getConnection();
-        if (conn != null) {
-            switch (action.getCommandType()) {
-                case JOIN_PLAYER:
-                    joinPlayer(new Gson().fromJson(message, JoinPlayer.class), session);
-                    break;
-                case JOIN_OBSERVER:
-                    joinObserver(new Gson().fromJson(message, JoinObserver.class), session);
-                    break;
-            }
-        } else {
-            session.getRemote().sendString(new Gson().toJson(new Error("Error: unknown user")));
+        switch (action.getCommandType()) {
+            case JOIN_PLAYER:
+                joinPlayer(new Gson().fromJson(message, JoinPlayer.class), session);
+                break;
+            case JOIN_OBSERVER:
+                joinObserver(new Gson().fromJson(message, JoinObserver.class), session);
+                break;
+            case MAKE_MOVE:
+                makeMove(new Gson().fromJson(message, MakeMove.class), session);
+                break;
+            case LEAVE:
+                leave(new Gson().fromJson(message, Leave.class), session);
+                break;
+            case RESIGN:
+                resign(new Gson().fromJson(message, Resign.class), session);
+                break;
         }
     }
 }
--- shared/src/main/java/webSocketMessages/userCommands/MakeMove.java
+++ shared/src/main/java/webSocketMessages/userCommands/MakeMove.java
@@ -1,12 +1,29 @@
 package webSocketMessages.userCommands;
 
+import chess.ChessPiece;
+import chess.ChessPosition;
+
 public class MakeMove extends UserGameCommand{
     String username;
-    public MakeMove(String authToken, String username) {
+    String id;
+    ChessPosition startPosition;
+    ChessPosition endPosition;
+    ChessPiece.PieceType promotion;
+    public MakeMove(String authToken, String username, String id, ChessPosition startPosition, ChessPosition endPosition, ChessPiece.PieceType promotion) {
         super(authToken);
         this.commandType = CommandType.MAKE_MOVE;
         this.username = username;
+        this.id = id;
+        this.startPosition = startPosition;
+        this.endPosition = endPosition;
+        this.promotion = promotion;
     }
 
     public String getUsername(){return username;}
+
+    public String getId(){return id;}
+
+    public ChessPosition getStartPosition(){return startPosition;}
+    public ChessPosition getEndPosition(){return endPosition;}
+    public ChessPiece.PieceType getPromotion(){return promotion;}
 }
